82676c072125f25d1bf4c66f0b1fb7ee8397568a
John <john@springframework.guru>
2019-04-15 22:17:19 -0400

    initial add of project

---
+++ src/test/java/sfg/beerworks/pub/PubApplicationTests.java
@@ -0,0 +1,14 @@
+package sfg.beerworks.pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
